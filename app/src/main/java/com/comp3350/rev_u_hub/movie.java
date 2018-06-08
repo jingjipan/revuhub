cad2c55de3b8fa4071be2fb9a3966977c6abfafa
steve <panj3457@myumanitoba.ca>
2018-06-08 17:09:06 -0500

    Add interfaces for movie object.

--- app/src/main/java/com/comp3350/rev_u_hub/movie.java
+++ app/src/main/java/com/comp3350/rev_u_hub/movie.java
@@ -14,4 +14,36 @@ public class movie {
 	public void addReviews(String newReview){
 		reviews.add(newReview);
 	}
+
+	public String getMovieName(){
+		return movieName;
+	}
+
+	public String getIntro(){
+		return intro;
+	}
+
+	public List<String> getReviews(){
+		return reviews;
+	}
+
+	public String[] getPicsUrls(){
+		return pics;
+	}
+
+	public void changeMovieName(String movieName){
+		this.movieName=movieName;
+	}
+
+	public void changeReviews(List<String> reviews){
+		this.reviews=reviews;
+	}
+
+	public void changePicUrls(String[] pics){
+		this.pics=pics;
+	}
+
+	public void changeIntro(String intro){
+		this.intro=intro;
+	}
 }
