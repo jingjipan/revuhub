ae1bc8b1e2b6be9dd62fa2435f868713a8ff5ca0
Kiefer <com@myumanitoba.ca>
2018-06-11 23:00:08 -0500

    Sorted layers into packages.  Added an interface to Persistence, but otherwise didn't touch it.  Fleshed out some more Logic and DMO behaviour.

--- app/src/main/java/com/comp3350/rev_u_hub/LogicLayer/LogicInterface.java
+++ app/src/main/java/com/comp3350/rev_u_hub/LogicLayer/LogicInterface.java
@@ -8,23 +8,23 @@ import java.io.File;
 
 public interface LogicInterface {
     //As a user, I want to be able to view the synopsis of a movie. (high)
-    public String getSynopsis(MovieDMObject movie);
+    String getSynopsis(MovieDMObject movie);
 
     //As a user, I want to be able to view the casts of a movie. (high)
-    public ArrayList<String> getCast(MovieDMObject movie);
+    ArrayList<String> getCast(MovieDMObject movie);
 
     //As a user, I want to be able to view the photos of a movie. (high)
-    public File getPhoto(MovieDMObject movie); //returns file path to movie
+    File getPhoto(MovieDMObject movie); //returns file path to movie
 
     //As a user, I want to be able to view the news of a movie. (high)
-    public String getNews(MovieDMObject movie);
+    String getNews(MovieDMObject movie);
 
     //As a user, I want to be able to view reviews about a movie. (high)
-    public ReviewDMObject getReview(MovieDMObject movie);
+    ReviewDMObject getReview(MovieDMObject movie);
 
     //As a user, I want to be able to rate reviews about a movie. (high)
-    public boolean setReview(MovieDMObject movie, ReviewDMObject review);
+    boolean setReview(MovieDMObject movie, ReviewDMObject review);
 
     //As a user, I want to be able to search movies based on title. (high)
-    public MovieDMObject getMovie(String title);
+    MovieDMObject getMovie(String title);
 }
