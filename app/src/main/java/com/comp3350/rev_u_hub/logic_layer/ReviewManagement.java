b792141dfd839c7ff7087ab8ff5791669d0c9ce2
Kiefer Co <com@myumanitoba.ca>
2018-07-05 12:31:29 -0500

    Added more logic unit tests and changed implementation of ReviewManagement

--- app/src/main/java/com/comp3350/rev_u_hub/logic_layer/ReviewManagement.java
+++ app/src/main/java/com/comp3350/rev_u_hub/logic_layer/ReviewManagement.java
@@ -14,15 +14,39 @@ import com.comp3350.rev_u_hub.logic_layer.exceptions.ReviewDataNoUserException;
 import com.comp3350.rev_u_hub.logic_layer.exceptions.ReviewDataNotFoundException;
 import com.comp3350.rev_u_hub.logic_layer.exceptions.ReviewDataWrongUserException;
 import com.comp3350.rev_u_hub.logic_layer.exceptions.UserDataNotFoundException;
+import com.comp3350.rev_u_hub.logic_layer.interfaces.MovieSearch;
 import com.comp3350.rev_u_hub.logic_layer.interfaces.ReviewManager;
+import com.comp3350.rev_u_hub.logic_layer.interfaces.ReviewSearch;
+import com.comp3350.rev_u_hub.logic_layer.interfaces.UserLogin;
+import com.comp3350.rev_u_hub.logic_layer.interfaces.UserSearch;
 import com.comp3350.rev_u_hub.persistence_layer.ReviewPersistence;
 
 public class ReviewManagement implements ReviewManager {
 
     private ReviewPersistence myPersistenceLayer;
+    private ReviewSearch reviewSearch;
+    private MovieSearch movieSearch;
+    private UserSearch userSearch;
+    private UserLogin userLogin;
 
     public ReviewManagement(ReviewPersistence setPersistenceLayer) {
         myPersistenceLayer = setPersistenceLayer;
+        reviewSearch = Services.getReviewSearch();
+        movieSearch = Services.getMovieSearch();
+        userSearch = Services.getUserSearch();
+        userLogin = Services.getUserLogin();
+    }
+
+    public ReviewManagement(ReviewPersistence setPersistenceLayer,
+                            ReviewSearch setReviewSearch,
+                            MovieSearch setMovieSearch,
+                            UserSearch setUserSearch,
+                            UserLogin setUserLogin) {
+        myPersistenceLayer = setPersistenceLayer;
+        reviewSearch = setReviewSearch;
+        movieSearch = setMovieSearch;
+        userSearch = setUserSearch;
+        userLogin = setUserLogin;
     }
 
     // Creates and stores a review if its parameters are valid
@@ -39,7 +63,7 @@ public class ReviewManagement implements ReviewManager {
         }
 
         try {
-            if (!user.equals(Services.getUserLogin().getUser()))
+            if (!user.equals(userLogin.getUser()))
                 throw new ReviewCreationNoUserException("The selected user is not logged in.");
         } catch (UserDataNotFoundException e) {
             throw new ReviewCreationNoUserException("No user is currently logged in.");
@@ -51,7 +75,7 @@ public class ReviewManagement implements ReviewManager {
             throw new ReviewCreationFailedException(e.getMessage());
         }
 
-        if (Services.getReviewSearch().reviewExists(movieName, userName))
+        if (reviewSearch.reviewExists(movieName, userName))
             throw new ReviewCreationDuplicateException("A review by the selected user for the "
                     + "selected movie already exists.");
 
@@ -59,7 +83,7 @@ public class ReviewManagement implements ReviewManager {
         myPersistenceLayer.addNewReview(review);
 
         try {
-            review = Services.getReviewSearch().getReview(movie, user);
+            review = reviewSearch.getReview(movie, user);
         } catch (ReviewDataNotFoundException e) {
             throw new ReviewCreationFailedException("The review could not be created.");
         }
@@ -77,7 +101,7 @@ public class ReviewManagement implements ReviewManager {
         user = getUserObject(userName);
 
         try {
-            if (!user.equals(Services.getUserLogin().getUser()))
+            if (!user.equals(userLogin.getUser()))
                 throw new ReviewDataWrongUserException("The selected user is not logged in.");
         } catch (UserDataNotFoundException e) {
             throw new ReviewDataNoUserException("No user is currently logged in.");
@@ -85,7 +109,7 @@ public class ReviewManagement implements ReviewManager {
 
         movie = getMovieObject(movieName);
 
-        review = Services.getReviewSearch().getReview(movie, user);
+        review = reviewSearch.getReview(movie, user);
 
         review.setReview(reviewName);
         myPersistenceLayer.updateReview(review);
@@ -95,12 +119,12 @@ public class ReviewManagement implements ReviewManager {
 
     // Removes a review if it exists
     public void removeReview(ReviewObject review) throws ReviewDataException {
-        myPersistenceLayer.deleteReview(Services.getReviewSearch().getReview(review.getMovieName(),
+        myPersistenceLayer.deleteReview(reviewSearch.getReview(review.getMovieName(),
                 review.getUserName()));
     }
 
     private MovieObject getMovieObject(String title) throws ReviewDataNoMovieException {
-        MovieObject movie = Services.getMovieSearch().getMovieSimple(title);
+        MovieObject movie = movieSearch.getMovieSimple(title);
 
         if (movie==null || movie.isEmpty())
             throw new ReviewDataNoMovieException("The selected movie does not exist.");
@@ -108,7 +132,7 @@ public class ReviewManagement implements ReviewManager {
     }
 
     private UserObject getUserObject(String userName) throws ReviewDataException {
-        UserObject user = Services.getUserSearch().getUserSimple(userName);
+        UserObject user = userSearch.getUserSimple(userName);
 
         if (user==null || user.isEmpty())
             throw new ReviewDataNoUserException("The selected user does not exist.");
